0e2abcb3ec32b745777f2aef9bdb7764d8d60785
Matthew Hawn <steamraven@yahoo.com>
2018-11-17 17:48:44 -0700

    Update the UI to make it more consistent * Allow export to be used directly * Export NoUserError * Fail Authentication on kerberos validation error instead of generating error

--- test/package.test.js
+++ test/package.test.js
@@ -7,4 +7,9 @@ describe('passport-negotiate', function (){
   it('should export Strategy constructor', function () {
     expect(negotiate.Strategy).to.be.a('function');
   });
+  it('should export Strategy constructor as a module', function () {
+    expect(negotiate).to.be.a('function');
+    expect(negotiate).to.be.equal(negotiate.Strategy);
+  })
+  
 });
